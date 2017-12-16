4ae95d4fea1731a56a3ce9937bb5af46ebdb6292
Peter Juenger <peter.juenger@hotmail.com>
2017-12-16 12:29:55 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# bmwcdapi.py 
+https://github.com/jupe76/
+
+Bmwcdapi.py is a python script to query various informations about your car from the BMW ConnectedDrive portal in conjunction with obenhab.
+
+I'm using it for a BMW i3, but should work with other types as well.
+
+The techniques to access the ConnectedDrive portal were taken from https://github.com/sergejmueller/battery.ebiene.de
+
+### Prerequisites
+You'll need the credentials (username / password) for a ConnectedDrive Login and the 17 chars long vehicle ident number (VIN) of your car.
+Furthermore you need a running openhab installation and python 3.x . The script has been tested with openhab2.
+
+### Installation
+Copy bmw.rules and bmw.items in the according openhab dirs.
+
+Edit bmwUsername, bmwPassword and bmwVin placed in bmw.items to hold your credentials and the VIN of your car.
+
+If your script dir is not /etc/openhab2/scripts than you need to edit bmw.rules.
+
+Copy bmwcdapi.py to the openhab script dir. If the script is running on another computer than your openhab installation, than you need to adjust OPENHABIP.
+
+Edit your sitemap to visualize the items.
+The script is called every 2 hours to update the values. You can adjust the timespan in bmw.rules (be nice to the ConnectedDrive portal and don't hammer the server)
+With the switch bmwForceUpdate you could initiate an immediate update.
+
+### Roadmap
+This is the first version of the script, it can only read values and has no support to initiate actions as to switch on heating or cooling.
+Maybe I'll try to add this features later.
---
+++ bmwcdapi.py
@@ -0,0 +1,129 @@
+#! /usr/bin/env python3
+# 
+# **** bmwcdapi.py ****
+# https://github.com/jupe76/bmwcdapi
+#
+# Query vehicle data from the BMW ConnectedDrive Website, i.e. for BMW i3
+# Based on the excellent work by Sergej Mueller
+# https://github.com/sergejmueller/battery.ebiene.de
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+import json
+import requests
+import time
+import urllib.parse
+import re
+
+# ADJUST HERE if OH is not running on "localhost:8080"
+OPENHABIP = "localhost:8080"
+
+# API Gateway
+AUTH_API = 'https://customer.bmwgroup.com/gcdm/oauth/authenticate'
+VEHICLE_API = 'https://www.bmw-connecteddrive.de/api/vehicle'
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0"
+
+class ConnectedDrive(object):
+
+    def __init__(self):
+        self.bmwUsername = self.ohGetValue("bmwUsername").json()["label"]
+        self.bmwPassword = self.ohGetValue("bmwPassword").json()["label"]
+        self.bmwVin = self.ohGetValue('bmwVin').json()["label"]
+        self.accessToken = self.ohGetValue('accessToken').json()["state"]
+        self.tokenExpires = self.ohGetValue('tokenExpires').json()["state"]
+
+        if((self.tokenExpires == 'NULL') or (int(time.time()) >= int(self.tokenExpires))):
+            self.generateCredentials()
+    
+    def generateCredentials(self):
+        """
+        If previous token has expired, create a new one.
+        """
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-agent": USER_AGENT
+        }
+
+        values = {'username' : self.bmwUsername,
+            'password' : self.bmwPassword,
+            'client_id' : 'dbf0a542-ebd1-4ff0-a9a7-55172fbfce35',
+            'redirect_uri' : 'https://www.bmw-connecteddrive.com/app/default/static/external-dispatch.html',
+            'response_type' : 'token',
+            'scope' : 'authenticate_user fupo',
+            'state' : 'eyJtYXJrZXQiOiJkZSIsImxhbmd1YWdlIjoiZGUiLCJkZXN0aW5hdGlvbiI6ImxhbmRpbmdQYWdlIn0',
+            'locale' : 'DE-de'
+        }
+
+        data = urllib.parse.urlencode(values)
+        r = requests.post(AUTH_API,  data=data, headers=headers,allow_redirects=False)
+        #statuscode will be 302
+        #print(r.status_code)
+
+        myPayLoad=r.headers['Location']
+        m = re.match(".*access_token=([\w\d]+).*token_type=(\w+).*expires_in=(\d+).*", myPayLoad )
+        
+        tokenType=(m.group(2))
+
+        self.accessToken=(m.group(1))
+        self.ohPutValue('accessToken',self.accessToken)
+
+        self.tokenExpires=int(time.time()) + int(m.group(3))
+        self.ohPutValue('tokenExpires',self.tokenExpires)
+
+    def ohPutValue(self, item, value):
+        requests.put('http://' + OPENHABIP + '/rest/items/'+ item +'/state', str(value))
+
+    def ohGetValue(self, item):
+        return requests.get('http://' + OPENHABIP + '/rest/items/'+ item)
+
+    def call(self):
+        headers = {
+            "Content-Type": "application/json",
+            "User-agent": USER_AGENT,
+            "Authorization" : "Bearer "+ self.accessToken
+            }
+        r = requests.get(VEHICLE_API+'/dynamic/v1/'+self.bmwVin+'?offset=-60', headers=headers,allow_redirects=True)
+
+        map=r.json()['attributesMap']
+        #for k, v in map.items():
+        #    print(k, v)
+        if('door_lock_state' in map):
+            self.ohPutValue("doorLockState",map['door_lock_state'])
+        if('chargingLevelHv' in map):
+            self.ohPutValue("chargingLevelHv",map['chargingLevelHv'])
+        if('beRemainingRangeElectric' in map):
+            self.ohPutValue("beRemainingRangeElectric",map["beRemainingRangeElectric"])
+        if('mileage' in map):
+            self.ohPutValue("mileage",map["mileage"])
+        if('beRemainingRangeFuel' in map):
+            self.ohPutValue("beRemainingRangeFuel",map["beRemainingRangeFuel"])
+        if('updateTime_converted_date' in map):
+            self.ohPutValue("updateTimeConverted", map['updateTime_converted_date']+ " " + map['updateTime_converted_time'])
+        if('chargingSystemStatus' in map):
+            self.ohPutValue("chargingSystemStatus", map['chargingSystemStatus'])
+
+        r = requests.get(VEHICLE_API+'/navigation/v1/'+self.bmwVin, headers=headers,allow_redirects=True)
+        map=r.json()
+        #for k, v in map.items():
+        #    print(k, v)
+        if('socMax' in map):
+            self.ohPutValue("socMax",map['socMax'])
+
+def main():
+    print("...running bmwcdapi.py")
+    c = ConnectedDrive()
+    c.call()
+
+if __name__ == '__main__':
+    main()
